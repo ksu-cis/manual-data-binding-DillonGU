5e59ac0edc1e5b007d3d5d48dd428ae4648a9d7d
Dillon Unruh <dillongunruh@ksu.edu>
2020-03-23 14:03:29 -0500

    Manual Data Binding Done

--- ManualDataBinding/UI/NoteEditor.xaml.cs
+++ ManualDataBinding/UI/NoteEditor.xaml.cs
@@ -19,9 +19,56 @@ namespace ManualDataBinding.UI
     /// </summary>
     public partial class NoteEditor : UserControl
     {
+        private Note note;
+        /// <summary>
+        /// The Note that will be edited
+        /// </summary>
+        public Note Note
+        {
+            get { return note; }
+            set
+            {
+                if (note == value) return;
+                if (note != null) note.NoteChanged -= OnNoteChange;
+                note = value;
+                if (note != null)
+                {
+                    note.NoteChanged += OnNoteChange;
+                    OnNoteChange(note, new EventArgs());
+                }
+            }
+        }
         public NoteEditor()
         {
             InitializeComponent();
+
+        }
+
+        public void OnNoteChange(object sender, EventArgs e)
+        {
+            if (Note == null)
+                return;
+            Title.Text = Note.Title;
+            Body.Text = Note.Body;
+        }
+        /// <summary>
+        /// Event handler for when the title changes
+        /// </summary>
+        /// <param name="sender">The TextBox that changed</param>
+        /// <param name="e">The event args</param>
+        public void OnTitleChanged(object sender, TextChangedEventArgs e)
+        {
+            Note.Title = Title.Text;
+        }
+
+        // <summary>
+        /// Event handler for when the body changes
+        /// </summary>
+        /// <param name="sender">The TextBox that changed</param>
+        /// <param name="e">The event args</param>
+        public void OnBodyChanged(object sender, TextChangedEventArgs e)
+        {
+            Note.Body = Body.Text;
         }
     }
 }
